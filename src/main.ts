99287689b294aa36b2845f9c2e4627100a239101
Gaidamavičius <marius.gaidamavicius@thermofisher.com>
2022-06-01 09:57:46 +0300

    2nd task exercises completed

--- src/main.ts
+++ src/main.ts
@@ -1,158 +1,52 @@
-type Person = {
-  id: string,
-  name: string,
-  surname: string,
-  age: number,
-  height?: number,
-  weight?: number,
-};
-
-const numData: number[] = [1, 2, 3, 4, 5, 6];
-const names: Array<string> = ['Jagnita', 'Kimparas', 'Pitonkas', 'Fasalija'];
-const people: Person[] = [{
-  id: '39304075689',
-  name: 'Verundijus',
-  surname: 'Bauda',
-  age: 51,
-}, {
-  id: '39304075689',
-  name: 'Ryja',
-  surname: 'Žaneirytė',
-  age: 41,
-  height: 1.65,
-  weight: 55,
-}, {
-  id: '39304075689',
-  name: 'Brudas',
-  surname: 'Veilokas',
-  age: 11,
-  height: 1.45,
-  weight: 45,
-}];
-
-type CreatePeopleArrayFunction = (p1: Person, p2: Person) => Person[];
-
-const printStrings = (strings: string[]): void => {
-  const printString = (str: string): void => console.log(str);
-
-  strings.forEach(printString);
-};
-
-const sumNumbers = (nums: Array<number>): number => {
-  const numberSumReducer = (sum: number, num: number): number => sum + num;
-
-  return nums.reduce(numberSumReducer, 0);
-};
-
-const createPeopleArray: CreatePeopleArrayFunction = (p1, p2) => [p1, p2];
-
-console.group('Panaudojimo pavyzdžiai:');
+console.group('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
 {
-  console.group('printStrings');
-  {
-    printStrings(names);
-  }
-  console.groupEnd();
+  type Drink = {
+    name: string,
+    price: number,
+  };
 
-  console.group('sumNumbers');
-  {
-    const result: number = sumNumbers(numData);
-    console.log({
-      numbers: numData,
-      result,
-    });
-  }
-  console.groupEnd();
+  const sortDrinksByPriceASC = (drink1: Drink, drink2: Drink): number => drink1.price - drink2.price;
 
-  console.group('createPeopleArray');
-  {
-    const couple: Array<Person> = createPeopleArray(people[0], people[1]);
-    console.log(couple);
-  }
-  console.groupEnd();
+  const solution = (drinks: Array<Drink>): Array<Drink> => [...drinks].sort(sortDrinksByPriceASC);
+
+  const drinks: Drink[] = [
+    { name: 'lemonade', price: 50 },
+    { name: 'fanta', price: 30 },
+    { name: 'lime', price: 10 },
+  ];
+
+  const sortedDrinks: Drink[] = solution(drinks);
+
+  console.log({
+    drinks,
+    sortedDrinks,
+  });
 }
 console.groupEnd();
 
-console.group('Užduotys');
+console.group('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
 {
-  console.group('1. Aprašykite funkcijoms ir kintamiesiems tipus');
-  {
-    const numbers: number[] = [1, -8, -6, 7, 5, 1];
+  type ItemObj = {
+    [key: string]: number,
+  };
 
-    function addPositiveNumbers(arr: number[]) {
-      const positiveNumberReducer = (sum: number, num: number) => (num > 0 ? sum + num : sum);
+  const solution = (itemObj: ItemObj, itemName: string, itemValue: number): ItemObj => ({
+    ...itemObj,
+    [itemName]: itemValue,
+  });
 
-      return arr.reduce<number>(positiveNumberReducer, 0);
-    }
+  type SolutionArgs = [ItemObj, string, number];
 
-    console.log({
-      numbers,
-      sumOfPositiveNumbers: addPositiveNumbers(numbers),
-    });
-  }
-  console.groupEnd();
+  const solutionArgs1: SolutionArgs = [{}, 'Brutus', 300];
+  const solutionArgs2: SolutionArgs = [{ piano: 500 }, 'Brutus', 400];
+  const solutionArgs3: SolutionArgs = [{ piano: 500, stereo: 300 }, 'Caligula', 440];
 
-  console.group('2. Sukurkite ir tipais aprašykite funkciją, kuri sudarytų string\'ą iš string\'ų masyvo elementų pirmųjų raidžių');
-  {
-    const acronymReducer = (acronym: string, word: string) => acronym + word[0];
+  const result1: ItemObj = solution(...solutionArgs1);
+  const result2: ItemObj = solution(...solutionArgs2);
+  const result3: ItemObj = solution(...solutionArgs3);
 
-    const createAcronym = (words: string[]): string => {
-      const acronym = words.reduce<string>(acronymReducer, '');
-      return acronym;
-    };
-
-    const dataSamples: string[][] = [
-      ['Lietuviškas', 'Nepriklausomas', 'Kanalas'],
-      ['Lietuvos', 'Respublikos', 'Televizija'],
-      ['Loughing', 'Out', 'Loud'],
-    ];
-
-    dataSamples.forEach((words: string[]) => console.log(`[${words.join(', ')}] -> ${createAcronym(words)}`));
-  }
-  console.groupEnd();
-
-  console.group('3. Sukurkite ir tipais aprašykite funkciją, kuri saudaugintų visus number masyvo skaičius');
-  {
-    type NumbersMultiplyReducer = (product: number, factor: number) => number;
-    type MultiplyNumbers = (numbers: Array<number>) => number;
-
-    const numbersProductReducer: NumbersMultiplyReducer = (product, factor) => product * factor;
-
-    const multiplyNumbers: MultiplyNumbers = (numbers) => numbers.reduce<number>(numbersProductReducer, 1);
-
-    type AnswerObject = {
-      [key: string]: number,
-    };
-
-    type FormatAnswerObject = (samples: Array<Array<number>>) => AnswerObject;
-
-    type AnswerObjectReducer = (answerObject: AnswerObject, numbers: Array<number>) => AnswerObject;
-
-    const samples: Array<Array<number>> = [
-      [1, 7, 8],
-      [98, 74, 5, 0],
-      [17, 10, 5],
-    ];
-
-    const answerObjectReducer: AnswerObjectReducer = (answerObject, numbers) => {
-      const functionName: string = multiplyNumbers.name;
-      const functionArgsString: string = `[${numbers.join(', ')}]`;
-      const key: string = `${functionName}(${functionArgsString})`;
-      const value: number = multiplyNumbers(numbers);
-      answerObject[key] = value;
-      return answerObject;
-    };
-
-    const formatAnswerObject: FormatAnswerObject = (samples) => {
-      const initialValue: AnswerObject = {};
-      const result: AnswerObject = samples.reduce<AnswerObject>(answerObjectReducer, initialValue);
-      return result;
-    };
-
-    const answerObject: AnswerObject = formatAnswerObject(samples);
-
-    console.log(answerObject);
-  }
-  console.groupEnd();
+  console.log(solutionArgs1, result1);
+  console.log(solutionArgs2, result2);
+  console.log(solutionArgs3, result3);
 }
 console.groupEnd();
