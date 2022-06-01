73fc5fc63ac4272b74f4002bad4452d4dc416f77
Gaidamavičius <marius.gaidamavicius@thermofisher.com>
2022-06-01 09:51:44 +0300

    2nd task objects completed

--- public/js/main.js
+++ public/js/main.js
@@ -1,63 +1,61 @@
 "use strict";
-const addNumbers = (num1, num2) => num1 + num2;
-const joinStrings = (str1, str2) => `${str1} ${str2}`;
-const createInitials = function (str1, str2) {
-    return str1[0].toLocaleUpperCase() + str2[0].toLocaleUpperCase();
+const person1 = {
+    id: '39304075689',
+    name: 'Verundijus',
+    surname: 'Bauda',
+    age: 51,
 };
-const sum = addNumbers(5, 7);
-const joinedString = joinStrings('Penediktas', 'Tušinis');
-const initials = createInitials('Penediktas', 'Tušinis');
-console.log({
-    sum,
-    joinedString,
-    initials,
-});
-function printRedText(text) {
-    console.log(`%c ${text}`, 'color: #ee0000');
-}
-printRedText('printRed funkcijos argumentas');
-console.group('1. Aprašykite funkcijoms tipus jas deklaruojant');
+const person2 = {
+    id: '39304075689',
+    name: 'Ryja',
+    surname: 'Žaneirytė',
+    age: 41,
+    height: 1.65,
+    weight: 55,
+};
+const person3 = {
+    id: '39304075689',
+    name: 'Brudas',
+    surname: 'Veilokas',
+    age: 11,
+    height: 1.45,
+    weight: 45,
+};
+const createFullname = ({ name, surname }) => `${name} ${surname}`;
+const printCouple = (p1, p2) => {
+    const p1Fullname = createFullname(p1);
+    const p2Fullname = createFullname(p2);
+    console.log(`${p1Fullname} + ${p2Fullname} = ❤`);
+};
+printCouple(person1, person2);
+console.group('1. Sukurkite funkciją kuri patikrina ar žmogus yra pilnametis');
 {
-    const multiply = (a, b) => a * b;
-    const power = function (base, power) {
-        return base ** power;
-    };
-    const squareRoot = (number) => number ** (1 / 2);
-    const root = function (base, nthRoot) {
-        return base ** (1 / nthRoot);
-    };
-    const printBlueText = (text) => console.log(`%c${text}`, 'color: #0000ee');
-    const num1 = 16;
-    const num2 = 4;
+    const isAdult = (p) => p.age >= 18;
     console.log({
-        [`multiply(${num1}, ${num2})`]: multiply(num1, num2),
-        [`power(${num1}, ${num2})`]: power(num1, num2),
-        [`squareRoot(${num1})`]: squareRoot(num1),
-        [`root(${num1}, ${num2})`]: root(num1, num2),
+        [createFullname(person1)]: isAdult(person1),
+        [createFullname(person2)]: isAdult(person2),
+        [createFullname(person3)]: isAdult(person3),
     });
-    printBlueText('printBlueText funkcijos argumentas');
 }
 console.groupEnd();
-console.group('2. Aprašykite funkcijų tipus prieš deklaruojant funkcijas');
+console.group('2. Sukurkite funkciją, kuri patikrina ar Person tipo objektas turi ūgį ir svorį');
 {
-    const multiply = (a, b) => a * b;
-    const power = function (base, power) {
-        return base ** power;
-    };
-    const squareRoot = (number) => number ** (1 / 2);
-    const root = function (base, nthRoot) {
-        return base ** (1 / nthRoot);
-    };
-    const printBlueText = (text) => console.log(`%c${text}`, 'color: #0000ee');
-    const num1 = 16;
-    const num2 = 4;
+    const isFullyDescribedPerson = (person) => Boolean(person.height) && Boolean(person.weight);
     console.log({
-        [`multiply(${num1}, ${num2})`]: multiply(num1, num2),
-        [`power(${num1}, ${num2})`]: power(num1, num2),
-        [`squareRoot(${num1})`]: squareRoot(num1),
-        [`root(${num1}, ${num2})`]: root(num1, num2),
+        [createFullname(person1)]: isFullyDescribedPerson(person1),
+        [createFullname(person2)]: isFullyDescribedPerson(person2),
+        [createFullname(person3)]: isFullyDescribedPerson(person3),
     });
-    printBlueText('printBlueText funkcijos argumentas');
+}
+console.groupEnd();
+console.group('3. Sukurkite funkciją, kuri grąžina žmogaus incialus');
+{
+    const createInitials = (p) => p.name[0] + p.surname[0];
+    console.log({
+        [createFullname(person1)]: createInitials(person1),
+        [createFullname(person2)]: createInitials(person2),
+        [createFullname(person3)]: createInitials(person3),
+    });
 }
 console.groupEnd();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,86 +1,82 @@
-const addNumbers = (num1: number, num2: number): number => num1 + num2;
-  type binaryStringBuilder = (str1: string, str2: string) => string;
-
-const joinStrings: binaryStringBuilder = (str1, str2) => `${str1} ${str2}`;
-
-const createInitials: binaryStringBuilder = function (str1, str2) {
-  return str1[0].toLocaleUpperCase() + str2[0].toLocaleUpperCase();
+type Person = {
+  id: string,
+  name: string,
+  surname: string,
+  age: number,
+  height?: number,
+  weight?: number,
 };
 
-const sum: number = addNumbers(5, 7);
-const joinedString: string = joinStrings('Penediktas', 'Tušinis');
-const initials: string = createInitials('Penediktas', 'Tušinis');
+const person1: Person = {
+  id: '39304075689',
+  name: 'Verundijus',
+  surname: 'Bauda',
+  age: 51,
+};
 
-console.log({
-  sum,
-  joinedString,
-  initials,
-});
+const person2: Person = {
+  id: '39304075689',
+  name: 'Ryja',
+  surname: 'Žaneirytė',
+  age: 41,
+  height: 1.65,
+  weight: 55,
+};
 
-function printRedText(text: string): void {
-  console.log(`%c ${text}`, 'color: #ee0000');
-}
+const person3: Person = {
+  id: '39304075689',
+  name: 'Brudas',
+  surname: 'Veilokas',
+  age: 11,
+  height: 1.45,
+  weight: 45,
+};
 
-printRedText('printRed funkcijos argumentas');
+type CreateFullname = (person: Person) => string;
+const createFullname: CreateFullname = ({ name, surname }) => `${name} ${surname}`;
 
-console.group('1. Aprašykite funkcijoms tipus jas deklaruojant');
+const printCouple = (p1: Person, p2: Person): void => {
+  const p1Fullname = createFullname(p1);
+  const p2Fullname = createFullname(p2);
+  console.log(`${p1Fullname} + ${p2Fullname} = ❤`);
+};
+
+printCouple(person1, person2);
+
+console.group('1. Sukurkite funkciją kuri patikrina ar žmogus yra pilnametis');
 {
-  const multiply = (a: number, b: number): number => a * b;
-
-  const power = function (base: number, power: number): number {
-    return base ** power;
-  };
-
-  const squareRoot = (number: number): number => number ** (1 / 2);
-
-  const root = function (base: number, nthRoot: number): number {
-    return base ** (1 / nthRoot);
-  };
-
-  const printBlueText = (text: string): void => console.log(`%c${text}`, 'color: #0000ee');
-
-  const num1: number = 16;
-  const num2: number = 4;
+  const isAdult = (p: Person): boolean => p.age >= 18;
 
   console.log({
-    [`multiply(${num1}, ${num2})`]: multiply(num1, num2),
-    [`power(${num1}, ${num2})`]: power(num1, num2),
-    [`squareRoot(${num1})`]: squareRoot(num1),
-    [`root(${num1}, ${num2})`]: root(num1, num2),
+    [createFullname(person1)]: isAdult(person1),
+    [createFullname(person2)]: isAdult(person2),
+    [createFullname(person3)]: isAdult(person3),
   });
-  printBlueText('printBlueText funkcijos argumentas');
 }
 console.groupEnd();
 
-console.group('2. Aprašykite funkcijų tipus prieš deklaruojant funkcijas');
+console.group('2. Sukurkite funkciją, kuri patikrina ar Person tipo objektas turi ūgį ir svorį');
 {
-    type UnaryNumberProductNumberFunction = (number: number) => number;
-    type UnaryStringProductVoidFunction = (str: string) => void;
-    type BinaryNumberProductNumberFunction = (num1: number, num2: number) => number;
+  type IsFullyDescribedPerson = (p: Person) => boolean;
 
-    const multiply: BinaryNumberProductNumberFunction = (a, b) => a * b;
+  const isFullyDescribedPerson: IsFullyDescribedPerson = (person) => Boolean(person.height) && Boolean(person.weight);
 
-    const power: BinaryNumberProductNumberFunction = function (base, power) {
-      return base ** power;
-    };
+  console.log({
+    [createFullname(person1)]: isFullyDescribedPerson(person1),
+    [createFullname(person2)]: isFullyDescribedPerson(person2),
+    [createFullname(person3)]: isFullyDescribedPerson(person3),
+  });
+}
+console.groupEnd();
 
-    const squareRoot: UnaryNumberProductNumberFunction = (number) => number ** (1 / 2);
+console.group('3. Sukurkite funkciją, kuri grąžina žmogaus incialus');
+{
+  const createInitials = (p: Person) => p.name[0] + p.surname[0];
 
-    const root: BinaryNumberProductNumberFunction = function (base, nthRoot) {
-      return base ** (1 / nthRoot);
-    };
-
-    const printBlueText: UnaryStringProductVoidFunction = (text) => console.log(`%c${text}`, 'color: #0000ee');
-
-    const num1: number = 16;
-    const num2: number = 4;
-
-    console.log({
-      [`multiply(${num1}, ${num2})`]: multiply(num1, num2),
-      [`power(${num1}, ${num2})`]: power(num1, num2),
-      [`squareRoot(${num1})`]: squareRoot(num1),
-      [`root(${num1}, ${num2})`]: root(num1, num2),
-    });
-    printBlueText('printBlueText funkcijos argumentas');
+  console.log({
+    [createFullname(person1)]: createInitials(person1),
+    [createFullname(person2)]: createInitials(person2),
+    [createFullname(person3)]: createInitials(person3),
+  });
 }
 console.groupEnd();
